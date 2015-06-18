5f8ba03b9ce74f4ce3c5616e28c74b8dba4eaebc
toecto <toecto@gmail.com>
2015-06-18 02:32:48 -0400

    Argumented service factory

--- src/ServiceContainer.php
+++ src/ServiceContainer.php
@@ -3,49 +3,24 @@
 namespace Reactor\ServiceContainer;
 
 
-class ServiceContainer {
-
-    protected $data = array();
-
-    public function setAll($data) {
-        $this->data = $data;
-    }
-
-    public function has($name) {
-        return isset($this->data[$name]);
-    }
+class ServiceContainer extends ValueContainer {
 
     public function createService($name, $value = null) {
         if (is_a($value, 'Reactor\\ServiceContainer\\ServiceProviderInterface')) {
-            return $this->data[$name] = $value;
+            $value = new ServiceProvider($value);
         }
-        return $this->data[$name] = new ServiceProvider($value);
-    }
-    public function set($name, $value = null) {
         return $this->data[$name] = $value;
     }
 
     public function get($name) {
-        if (!isset($this->data[$name])) {
+        $value = $this->getValue($name);
+        if ($value === null) {
             throw new Exceptions\ServiceNotFoundExeption($name);
         }
-        $value = $this->data[$name];
         if (is_a($value, 'Reactor\\ServiceContainer\\ServiceProviderInterface')) {
             return $value->get($this);
         }
         return $value;
-    }
-
-    public function __get($name) {
-        return $this->get($name);
-    }
-
-    public function __set($name, $value) {
-        return $this->set($name, $value);
-    }
-
-    public function __isset($name) {
-        return $this->has($name);
     }
 
 }
